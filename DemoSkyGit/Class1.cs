570146ada630dcc8a075ee38e7f3bb5d7696feb7
ttlong <ttl1601@gmail.com>
2016-07-21 10:55:18 +0700

    add class

---
+++ DemoSkyGit/Class1.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace DemoSkyGit
+{
+    public class Class1
+    {
+        //create class
+
+    }
+}
